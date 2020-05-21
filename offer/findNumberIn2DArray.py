c7947accd46e235153745f8ea6cf665aded2a88e
craig.wu <youzidian@hotmail.com>
2020-05-21 17:55:15 +0800

    leetcode 20200521

---
+++ offer/findNumberIn2DArray.py
@@ -0,0 +1,46 @@
+"""
+在一个 n * m 的二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+
+ 
+
+示例:
+
+现有矩阵 matrix 如下：
+
+[
+  [1,   4,  7, 11, 15],
+  [2,   5,  8, 12, 19],
+  [3,   6,  9, 16, 22],
+  [10, 13, 14, 17, 24],
+  [18, 21, 23, 26, 30]
+]
+给定 target = 5，返回 true。
+
+给定 target = 20，返回 false。
+
+"""
+
+class Solution:
+    def __init__(self, matrix, target):
+        self.matrix = matrix
+        self.target = target
+    def findNumberIn2DArray(self):
+        for list_a in matrix:
+            # print(list_a)
+            for i in list_a:
+                if target == i:
+                    return True
+        return False
+
+matrix = [
+  [1,   4,  7, 11, 15],
+  [2,   5,  8, 12, 19],
+  [3,   6,  9, 16, 22],
+  [10, 13, 14, 17, 24],
+  [18, 21, 23, 26, 30]
+]
+
+target = 5
+
+a = Solution(matrix,target)
+a.findNumberIn2DArray()
---
+++ offer/replaceSpace.py
@@ -0,0 +1,23 @@
+"""
+请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+
+ 
+
+示例 1：
+
+输入：s = "We are happy."
+输出："We%20are%20happy."
+
+"""
+
+class Solution:
+    def __init__(self,s):
+        self.s = s
+    def replaceSpace(self):
+        # print(123)
+        s.replace('a','%20')
+        print(s)
+
+s = "We are happy."
+a=Solution(s)
+a.replaceSpace()
