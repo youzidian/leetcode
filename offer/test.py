d3a673bd8c4ea0865006a35cfa2c4fb01462d9e2
craig.wu <youzidian@hotmail.com>
2020-06-30 17:50:38 +0800

    20200630

--- offer/test.py
+++ offer/test.py
@@ -1,5 +1,6 @@
 data = [4, [3, [2, [1]]]]
 
+
 def sum_list_num(data):
     list_num = 0
     for i in data:
@@ -9,6 +10,7 @@ def sum_list_num(data):
             list_num += i
     print(list_num)
     return list_num
+
 
 if __name__ == '__main__':
     print(sum_list_num(data))
