8f88e43f269d9496f9e5cf7c4804934358c69826
craig.wu <youzidian@hotmail.com>
2020-07-03 17:57:25 +0800

    20200793

--- self_learn/zen.py
+++ self_learn/zen.py
@@ -1,1 +1,9 @@
-import this
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+active = True
+while active:
+    message = input(prompt)
+    if message == 'quit':
+        active = False
+    else:
+        print(message)
